1869cfc432419d3cb20e327e0c1595a19c7827f2
aacevedoday <aacevedo@dayscript.com>
2018-09-07 18:20:02 -0500

    make block

---
+++ web/modules/custom/kokoriko/src/Plugin/Block/FullImageBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\kokoriko\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'FullImageBlock' block.
+ *
+ * @Block(
+ *  id = "full_image:block",
+ *  admin_label = @Translation("Full Image Block"),
+ * )
+ */
+class FullImageBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $results = 'adasdad';
+
+    $twig = \Drupal::service('twig');
+    $template = $twig->loadTemplate(drupal_get_path('module', 'kokoriko') . '/templates/full-image-block.html.twig');
+    $html = $template->render(['results'=> $results]);
+
+    $response['response'] = [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'response'],
+        '#markup' => $html
+      ];
+
+    return $response['response'];
+  }
+
+}
