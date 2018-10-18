426a2762f460438244d8b60304236bee7edcf96d
Juan Carlos Orrego <jcorrego@dayscript.com>
2018-10-18 03:47:12 +0000

    update

--- web/modules/custom/kokoriko/src/Plugin/Block/FullImageBlock.php
+++ web/modules/custom/kokoriko/src/Plugin/Block/FullImageBlock.php
@@ -19,31 +19,21 @@ class FullImageBlock extends BlockBase {
    */
   public function build() {
 
-    $results = 'adasdad';
-
-    $twig = \Drupal::service('twig');
-    $template = $twig->loadTemplate(drupal_get_path('module', 'kokoriko') . '/templates/full-image-block.html.twig');
-    $html = $template->render(['results'=> $results]);
-
-    // $response['response'] = [
-    //     '#type' => 'container',
-    //     '#attributes' => ['id' => 'response'],
-    //     '#markup' => $html
-    //   ];
-
-    // $response['response'];
+    // $results = 'adasdad';
+    //
+    // $twig = \Drupal::service('twig');
+    // $template = $twig->loadTemplate(drupal_get_path('module', 'kokoriko') . '/templates/full-image-block.html.twig');
+    // $html = $template->render(['results'=> $results]);
 
 
-    return [
+    //$variables
+    $return = [
       '#type' => 'html_tag',
       '#tag' => 'block-accompaniments', // Selector of the your app root component from the Angular app
       '#attributes' => ['id' => 'response'],
-    //  '#markup' => $html,
-      // '#attached' => [
-      //   'library' => [
-      //     'drupal_angular/drupal_angular_lib', // To load the library only with this block
-      //   ],
-      // ],
     ];
+
+    
+    return $return;
   }
 }
