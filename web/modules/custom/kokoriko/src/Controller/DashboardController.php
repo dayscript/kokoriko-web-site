5d6d2f5afeb83575fc7d78b47c24e076a12852a7
Juan Carlos Orrego <jcorrego@dayscript.com>
2018-10-19 21:01:04 +0000

    update

---
+++ web/modules/custom/kokoriko/src/Controller/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\kokoriko\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\Entity\User;
+
+/**
+ * Defines HelloController class.
+ */
+class DashboardController extends ControllerBase {
+
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+
+
+  public function Overview(){
+
+    $user = \Drupal\user\Entity\User::load(40);
+    $user = $user->field_no_identificacion->value;
+
+
+
+    $return = [
+      '#type' => 'html_tag',
+      '#tag' => 'block-accompaniments', // Selector of the your app root component from the Angular app
+      '#attributes' => ['id' => 'response'],
+    ];
+    $return['#attached']['drupalSettings']['kokoriko']['kokorikoJS'] = $user;
+
+    return $return;
+  }
+
+}
