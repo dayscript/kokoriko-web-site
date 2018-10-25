599c33f6c7041ad451353ed3a4df267722358ffc
aacevedoday <you@example.com>
2018-10-25 00:08:32 -0500

    update

--- web/modules/custom/kokoriko/src/Controller/DashboardController.php
+++ web/modules/custom/kokoriko/src/Controller/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends ControllerBase {
    public function __construct() {
      $this->account     = Drupal::currentUser();
      $this->user        = User::load( $this->account ->id() );
-     $this->user_fields = ['field_nombres','field_apellidos','field_mail','field_no_identificacion','field_nombres'];
+     $this->user_fields = ['field_nombres','field_apellidos','field_no_identificacion','field_nombres'];
      $this->output      = [];
    }
 
@@ -35,7 +35,7 @@ class DashboardController extends ControllerBase {
     $this->output['id'] = $this->account->id();
     $this->output['user_picture'] = '/themes/contrib/kokoriko_theme/images/default-user.jpg';
 
-    if( $this->user->get('user_picture')->entity->getFileUri() ){
+    if( $this->user->get('user_picture')->entity && $this->user->get('user_picture')->entity->getFileUri() ){
         $this->output['user_picture'] = str_replace('public://','/sites/default/files/', $this->user->get('user_picture')->entity->getFileUri());
     }
 
