5d821a558b14ff6d1559a39dab13115d8dafc27d
aacevedoday <aacevedo@dayscript.com>
2018-10-25 12:34:25 -0500

    update

---
+++ web/themes/contrib/kokoriko_theme/js/scripts.js
@@ -0,0 +1,531 @@
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a#menu-contacto').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('nav a#menu-domicilio').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a#menu-contacto').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('nav a#menu-domicilio').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a ').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a ').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block .body a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block ').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('#block-domicilios-block ').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+"use strict";
+
+function cosas() {}
+'use strict';
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+
+  $('nav a').click(function (e) {
+    e.preventDefault(); //evitar el eventos del enlace normal
+    var strAncla = $(this).attr('href'); //id del ancla
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAncla).offset().top
+    }, 1000);
+  });
+})(jQuery, Drupal);
+'use strict';
+
+function cosas() {}
+
+$('nav a').click(function (e) {
+  e.preventDefault(); //evitar el eventos del enlace normal
+  var strAncla = $(this).attr('href'); //id del ancla
+  $('body,html').stop(true, true).animate({
+    scrollTop: $(strAncla).offset().top
+  }, 1000);
+});
+"use strict";
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+})(jQuery, Drupal);
+'use strict';
+
+function cosas() {}
+
+$('nav a').click(function (e) {
+  e.preventDefault(); //evitar el eventos del enlace normal
+  var strAncla = $(this).attr('href'); //id del ancla
+  $('body,html').stop(true, true).animate({
+    scrollTop: $(strAncla).offset().top
+  }, 1000);
+});
+"use strict";
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+})(jQuery, Drupal);
+'use strict';
+
+function cosas() {}
+
+$('nav a').click(function (e) {
+  e.preventDefault(); //evitar el eventos del enlace normal
+  var strAncla = $(this).attr('href'); //id del ancla
+  $('body,html').stop(true, true).animate({
+    scrollTop: $(strAncla).offset().top
+  }, 1000);
+});
+"use strict";
+
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function attach(context, settings) {
+      //alert("I'm alive!");
+    }
+  };
+})(jQuery, Drupal);
