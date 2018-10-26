08b18afc304ee5b94bb62e1ae0e185b2da1fb28e
aacevedoday <you@example.com>
2018-10-26 16:08:11 -0500

    update

--- web/modules/custom/kokoriko/src/Routing/RouteSubscriber.php
+++ web/modules/custom/kokoriko/src/Routing/RouteSubscriber.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\user\Entity\User;
 
 
 /**
@@ -22,11 +23,20 @@ class RouteSubscriber implements EventSubscriberInterface {
   public function __construct() {
     $this->account = \Drupal::currentUser();
     $this->current_path = (\Drupal::service('path.current')->getPath() == '/kokoripesos');
+    $this->dashboard_path = (\Drupal::service('path.current')->getPath() == '/user/dashboard');
     $this->user_profile_path = (\Drupal::service('path.current')->getPath() == '/user');
 
   }
 
   public function checkAuthStatus(GetResponseEvent $event) {
+
+    if( $this->dashboard_path ){
+      if( !User::load( $this->account ->id() )->get('field_no_identificacion')->value ){
+        drupal_set_message(t('Por favor actualiza tus datos.'));
+        $response = new RedirectResponse('/user/' . $this->account ->id() . '/edit', 301);
+        $event->setResponse($response);
+      }
+    }
 
 
     if ( !$this->account->isAnonymous() && ( $this->current_path || $this->user_profile_path ) ) {
