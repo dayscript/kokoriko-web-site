1742bf4ba433a97e9d9d826c9cd46ce0e190d206
aacevedoday <you@example.com>
2018-11-03 15:44:26 -0500

    event facebbok

--- web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
+++ web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
@@ -68,8 +68,8 @@ class KokorikoSubscriber implements EventSubscriberInterface {
   public function modifyPermissionScope(GenericEvent $event) {
     $scope = $event->getArgument('scope');
     // Add the permission here. In this example we add 'public_profile'.
-    // $scope[] = 'public_profile';
-    $scope[] = 'user_birthday';
+    $scope[] = 'public_profile';
+    
     $event->setArgument('scope', $scope);
   }
 
