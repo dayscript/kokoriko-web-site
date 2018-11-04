1f60a5be86ca7668b7989050285e42d1041eff4a
aacevedoday <you@example.com>
2018-11-04 10:45:10 -0500

    update

--- web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
+++ web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
@@ -110,7 +110,7 @@ class KokorikoSubscriber implements EventSubscriberInterface {
     // user's Facebook access token, which SimpleFbConnect has stored to session
     // for other modules.
     $access_token = $this->persistentDataHandler->get('access_token');
-
+    dvm($access_token);
     if ($access_token) {
       try {
         $facebook_profile_fields = [
@@ -125,7 +125,7 @@ class KokorikoSubscriber implements EventSubscriberInterface {
         ];
 
         $graph_node = $this->facebook->get('/me?fields='.implode($facebook_profile_fields,","), $access_token)->getGraphNode();
-        dvm($graph_node);
+
         $name = $graph_node->getField('name') ;
 
         if( $name ){
