942be4d09b506c5a5071ec4070cb4cb2f036f3e9
aacevedoday <you@example.com>
2018-11-04 12:02:26 -0500

    styles

--- web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
+++ web/modules/custom/kokoriko/src/EventSubscriber/KokorikoSubscriber.php
@@ -90,7 +90,7 @@ class KokorikoSubscriber implements EventSubscriberInterface {
     // Enter your own code here. Remember to save the user with $user->save()
     // if you modify the user object.
     $message = t('Por activar tu cuenta, te hemos obsequiado 50 kokoripesos, actualiza tus datos para disfrutarlos.');
-    drupal_set_message($message, 'status');
+    drupal_set_message($message);
   }
   /**
    * Reacts to the event when user logs in via Simple FB Connect.
