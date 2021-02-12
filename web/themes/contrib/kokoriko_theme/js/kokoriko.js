7be7d350c46a4e2b1715a46329ddf95fb96b39a0
cesar <csaralbrto@gmail.com>
2021-02-12 14:20:17 -0500

    ajustes css y js mitimiti

--- web/themes/contrib/kokoriko_theme/js/kokoriko.js
+++ web/themes/contrib/kokoriko_theme/js/kokoriko.js
@@ -46,6 +46,15 @@
 
   });
 
+  const path = window.location.pathname;
+  if (path.indexOf('/menu#elige_y_combina') >= 0){
+    console.log('se accedió a menu#elige y comparte')
+    $("#edit-field-categoria-de-producto-target-id-84").prop("checked", true);
+    $('input[id*="edit-submit-spaces"]').once().click();
+    $('input[id*="edit-submit-spaces"]').once().trigger('mousedown');
+  }
+  $('#edit-submit-menu').css('visibility','hidden')
+
   // $('#block-domicilios-block .body a').click(function(e){
   //   e.preventDefault();		//evitar el eventos del enlace normal
   //   var strAncla=$(this).attr('href'); //id del ancla
